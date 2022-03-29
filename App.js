c48cd7e508b04f70bf1e4210025bdf3d47cd3035
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-03-29 07:07:23 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import api from "./components/api";
+import axios from "axios";
+
+export default function App() {
+  return (
+    <>
+      <Homepage />
+      <api />
+    </>
+  );
+}
---
+++ src/components/Homepage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import StarWarsLogo from "../images/StarWarsLogo.jpg";
+
+export default function Homepage() {
+  return (
+    <img src={StarWarsLogo} alt="Star Wars Logo" className="starWarsLogo" />
+  );
+}
